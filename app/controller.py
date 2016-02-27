0249c5ac49bf19b9a2d5db71837e3f4315573572
agraham3 <agraham3@cougars.ccis.edu>
2016-02-27 14:14:50 -0600

    I have have a unique priority numbers for feature requests. I changed my database model.

--- app/controller.py
+++ app/controller.py
@@ -1,13 +1,45 @@
 from app import db
-from .models import FeatureRequestDB
+from .models import ClientTable, FeatureRequestTable, ProductAreaTable
+
+def priorityNumberReOrder(form, clientID):
+  clientRequests = FeatureRequestTable.query.filter(FeatureRequestTable.clientID == clientID).order_by('priorityNumber').all()
+  nxtNum = form.clientPriority.data + 1
+  for item in clientRequests:
+    pNum = item.priorityNumber
+    if pNum >= form.clientPriority.data:
+      if pNum + 1 == nxtNum:
+        item.priorityNumber += 1
+        nxtNum = item.priorityNumber + 1
+      else:
+        break
 
 def insert_db(form):
-  featureRequest = FeatureRequestDB(title = form.title.data,
-                                    description = form.description.data,
-                                    client = form.client.data,
-                                    clientPriority = form.clientPriority.data,
-                                    targetDate = form.targetDate.data,
-                                    ticketURL = form.ticketURL.data, 
-                                    productArea = form.productArea.data)
+  c = form.client.data
+  ClientTB = ClientTable.query.filter(ClientTable.name == c).first()
+  if not ClientTB:
+    client = ClientTable(name = c)    
+    db.session.add(client)
+
+  pa = form.productArea.data
+  ProductAreaTB = ProductAreaTable.query.filter(ProductAreaTable.name == pa).first()
+  if not ProductAreaTB:
+    product = ProductAreaTable(name=pa)
+    db.session.add(product)
+
+
+  ClientTB = ClientTable.query.filter(ClientTable.name == c).first()
+  ProductAreaTB = ProductAreaTable.query.filter(ProductAreaTable.name == pa).first()
+  clientID = ClientTB.id
+  productAreaID = ProductAreaTB.id
+  featureRequest = FeatureRequestTable(title = form.title.data,
+                                       description = form.description.data,
+                                       ticketURL = form.ticketURL.data,
+                                       priorityNumber = form.clientPriority.data,
+                                       targetDate = form.targetDate.data,
+                                       clientID = clientID,
+                                       productAreaID = productAreaID)
+
+
+  priorityNumberReOrder(form, clientID)
   db.session.add(featureRequest)
   db.session.commit()
--- app/models.py
+++ app/models.py
@@ -1,24 +1,39 @@
 from app import db
 
-class FeatureRequestDB(db.Model):
+class ClientTable(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(10), index=True, unique=True)
+  request = db.relationship('FeatureRequestTable', backref='client')
+
+  def __repr__(self):
+    return "Client Name: %r" % (self.name)
+
+class ProductAreaTable(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(10), index=True, unique=True)
+  request = db.relationship('FeatureRequestTable', backref='productArea')
+
+  def __repr__(self):
+    return "Product Area: %r" % (self.name)
+
+class FeatureRequestTable(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(50), index=False, unique=False)
   description = db.Column(db.String(200), index=False, unique=False)
-  client = db.Column(db.String(10), index=True, unique=False)
-  clientPriority = db.Column(db.Integer, index=False, unique=False)
-  targetDate = db.Column(db.Date, index=False, unique=False)
   ticketURL = db.Column(db.String(200), index=False, unique=False)
-  productArea = db.Column(db.String(10), index=True, unique=False)
+  priorityNumber = db.Column(db.Integer, index=False, unique=False)
+  targetDate = db.Column(db.Date, index=False, unique=False)
+  clientID = db.Column(db.Integer, db.ForeignKey('client_table.id'))
+  productAreaID = db.Column(db.Integer, db.ForeignKey('product_area_table.id'))
 
   def __repr__(self):
-    p = "Title: %r\n \
-    Description: %r\n \
-    Client: %r\n \
-    Client Priority Number: %r\n \
-    Target Date: %r\n \
-    Ticket URL: %r\n \
-    Product Area: %r\n" % (self.title, self.description, 
-                         self.client, self.clientPriority,
-                         self.targetDate, self.ticketURL, 
-                         self.productArea)
-    return p
+    return "Title: %r\n \
+            Description: %r\n \
+            Ticket URL: %r\n \
+            Priority Number: %r\n \
+            Target Date: %r\n \
+            Client ID: %r\n \
+            Product Area ID: %r\n " % (self.title, self.description,
+                                       self.ticketURL, self.priorityNumber,
+                                       self.targetDate, self.clientID,
+                                       self.productAreaID)
